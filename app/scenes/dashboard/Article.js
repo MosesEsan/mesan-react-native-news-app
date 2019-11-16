11df492ec2daa74aa878753dede260036cf874f6
Moses Esan <mosesesan@hotmail.com>
2019-11-16 15:53:22 +0000

    Added Article Scene

---
+++ app/scenes/dashboard/Article.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+import { WebView } from 'react-native-webview';
+
+export default function Article(props) {
+    const {navigation} = props;
+    const article = navigation.getParam("article");
+
+    //==================================================================================================
+
+    return (
+        <WebView source={{ uri: article.url }}
+                 startInLoadingState={true}
+                 onError={() => alert("Failed to load article.")}
+                 renderLoading={() => <ActivityIndicator style={{paddingVertical: 8}}/>}/>
+    );
+};
+
+Article.navigationOptions = ({navigation}) => {
+    return {
+        title: `${navigation.getParam('title')}`
+    }
+};
